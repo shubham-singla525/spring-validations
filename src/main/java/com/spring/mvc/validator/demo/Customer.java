6c64a5ff7a80d1536a18524768938369266f5fcc
shubham-singla525 <manishsingla177@gmail.com>
2024-03-14 14:07:33 +0530

    added spring mvc code + thymeleaf basics

--- src/main/java/com/spring/mvc/validator/demo/Customer.java
+++ src/main/java/com/spring/mvc/validator/demo/Customer.java
@@ -1,15 +1,21 @@
 package com.spring.mvc.validator.demo;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
     private String firstName;
 
     @NotNull(message="is required")
     @Size(min=1, message="is required")
-    private String lastName;
+    private String lastName="";
+
+    @NotNull(message = "is required")
+    @Min(value = 0,message = "value should be greater than or equals to 0")
+    @Max(value = 10,message = "value should be less than or equals to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-z0-9A-Z]{5}",message = "only 5 chars/digits")
+    private String postalCode;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +31,22 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
 }
--- src/main/java/com/spring/mvc/validator/demo/CustomerController.java
+++ src/main/java/com/spring/mvc/validator/demo/CustomerController.java
@@ -1,17 +1,50 @@
 package com.spring.mvc.validator.demo;
 
 
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.beanvalidation.BeanValidationPostProcessor;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    //initbinder is advanced and powerful annotation provided by spring api
+    // it removes leading and trailing zeros from the request
+    // if data has only whitespaces it converts that to null
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        webDataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String getCustomer(Model theModel){
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
     }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult){
+
+        System.out.println("LastName |"+customer.getLastName()+"|");
+
+        if(bindingResult.hasErrors()){
+            return "customer-form";
+        }else{
+            return "customer-confirmation";
+        }
+    }
+
+
+
 
 }
